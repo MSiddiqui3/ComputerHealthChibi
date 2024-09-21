7798e4f5461301e3ee9a431ba5c3a4db3699380b
Mohammed <msiddiqui3@ggc.edu>
2024-09-21 14:46:22 -0400

    Initial Commit

---
+++ src/test/java/com/example/computerhealthchibi/ComputerHealthChibiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.computerhealthchibi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerHealthChibiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
